55b3c0fbeb7f83032691336ded3ed4e543db4a67
Domonkos Suranyi <suranyi.domi@gmail.com>
2017-12-12 18:11:57 +0100

    Position test added

---
+++ test/Position.cpp
@@ -0,0 +1,19 @@
+#include <negli/Position.hpp>
+#include <igloo/igloo.h>
+
+using namespace igloo;
+
+Context(A_newly_constructed_Position)
+{
+    Spec(Should_have_x_and_y_coordinates)
+    {
+        negli::Position pos = negli::Position(2,9);
+        Assert::That(pos.x, Equals(2));
+        Assert::That(pos.y, Equals(9));
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    return TestRunner::RunAllTests(argc, argv);
+}
