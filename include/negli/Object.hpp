6e0e912f6ae83e0b9dc758d2a54d44bd28224a37
Domonkos Suranyi <suranyi.domi@gmail.com>
2017-12-18 20:40:20 +0100

    first line ifndef added

--- include/negli/Object.hpp
+++ include/negli/Object.hpp
@@ -1,12 +1,17 @@
+#ifndef NEGLI_OBJECT_HPP_INC_
+#define NEGLI_OBJECT_HPP_INC_
+
 #include "Position.hpp"
 
 namespace negli
 {
 	class Object {
-	public:	
+	public:
 		Position pos;
 
 		Object(int x, int y) : pos(x,y) {}
 		~Object() {}
 	};
 }
+#endif
+
